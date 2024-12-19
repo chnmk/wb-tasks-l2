bfcb9f9201c60e771542e2de22b80ebf0b8f998e
chnmk <a2molchanov@ya.ru>
2024-12-19 15:14:39 +0300

    feat: finish dev01

--- develop/dev01/task_test.go
+++ develop/dev01/task_test.go
@@ -1,18 +1,54 @@
 package CurrentTime
 
-/*
 import (
-	"context"
+	"bytes"
+	"log"
+	"os"
+	"strings"
 	"testing"
-
+	"time"
 )
 
 func TestGetTime(t *testing.T) {
-    // Проверить STDOUT?
-    GetCurrentTime()
+	var buf bytes.Buffer
+	log.SetOutput(&buf)
 
-	if false {
-		t.Error("error")
+	t.Cleanup(func() { log.SetOutput(os.Stderr) }) // Возврат к значению по умолчанию
+
+	GetCurrentTime()
+
+	_, err := time.Parse(time.TimeOnly, strings.Split(buf.String(), " ")[1])
+	if err != nil {
+		t.Errorf("error: expected valid time format: %v", err)
 	}
 }
-*/
+
+func TestGetTimeTwice(t *testing.T) {
+	var buf1 bytes.Buffer
+	log.SetOutput(&buf1)
+
+	t.Cleanup(func() { log.SetOutput(os.Stderr) }) // Возврат к значению по умолчанию
+
+	GetCurrentTime()
+
+	time.Sleep(1 * time.Second)
+
+	var buf2 bytes.Buffer
+	log.SetOutput(&buf2)
+
+	GetCurrentTime()
+
+	t1, err := time.Parse(time.TimeOnly, strings.Split(buf1.String(), " ")[1])
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t2, err := time.Parse(time.TimeOnly, strings.Split(buf2.String(), " ")[1])
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if t2.Before(t1) {
+		t.Errorf("time 2 is before time 1")
+	}
+}
